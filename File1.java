f13e128bc703746198127297a8f798372265ce7d
Hsaikrishna <120639890+Hsaikrishna@users.noreply.github.com>
2023-01-20 22:38:24 +0530

    My first java file

--- File1.java
+++ File1.java
@@ -1,5 +1,5 @@
 public class Myclass{
   public static void main(String []args){
-    System.out.println("This is a second java file in git project...");
+    System.out.println("This is my first java file in git repository...");
   }
 }
